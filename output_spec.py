d6fa54242a142ec4b2c3f97bb9293ec401e423f9
Yuma Kajihara <kajimars.uts1@gmail.com>
2017-11-10 13:02:03 +0900

    add audio spec ipynb

---
+++ output_spec.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import glob
+import os, sys
+import numpy as np
+import matplotlib as mpl
+mpl.use('Agg')
+import librosa
+import librosa.display
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+
+mp3s = glob.glob("/data/mp3/videos/*/*/*/*.mp4.mp3")
+print len(mp3s)
+
+pbar = tqdm(total=len(mp3s))
+for mp3path in mp3s:
+    y, sr = librosa.load(mp3path)
+    S = librosa.feature.melspectrogram(y, sr=sr, n_mels=128)
+    log_S = librosa.logamplitude(S, ref_power=np.max)
+    my_dpi = 100.
+    plt.figure(figsize=(287./my_dpi, 229./my_dpi), dpi=my_dpi)
+    librosa.display.specshow(log_S, sr=sr)
+    plt.tight_layout()
+    specpath = "/data/mp3_spec/" + mp3path.split("/mp3/")[1].split(".mp3")[0] + ".jpg"
+    plt.savefig(specpath, bbox_inches="tight", pad_inches=0.0)
+    pbar.update(1)
+pbar.close()
+print "finished."
